c3b7d9d67bf88beff39962c029c4969a540e6638
Alexei Robyn <shados@shados.net>
2014-08-11 12:01:37 +1000

    Added services.openssh.allowed_users to configur sshd_config AllowUsers

--- system/ssh.nix
+++ system/ssh.nix
@@ -1,19 +1,37 @@
 { config, pkgs, ... }:
 
+with pkgs.lib;
+
+let
+  cfg = config.services.openssh;
+in
+
 {
-  services.openssh = {
-    enable = true;
-    # Some options for improved security
-    ports = [ 54201 ]; # Non-default port for security, SSH module automatically adds its ports to the FW
-    passwordAuthentication = false;
-    challengeResponseAuthentication = false;
-    permitRootLogin = "no";
-    extraConfig = 
-      ''
-        AllowUsers shados
+  options.services.openssh = {
+    allowed_users = mkOption {
+      description = ''
+        A list of the users allowed to log in via SSH.
       '';
+      default = [ "shados" ];
+      type = types.nullOr (types.listOf types.str);
+    };
   };
-  # Add Mosh & allow Mosh ports :)
-  environment.systemPackages = [ pkgs.mosh ];
-  networking.firewall.allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
+
+  config = {
+    services.openssh = {
+      enable = true;
+      # Some options for improved security
+      ports = [ 54201 ]; # Non-default port for security, SSH module automatically adds its ports to the FW
+      passwordAuthentication = false;
+      challengeResponseAuthentication = false;
+      permitRootLogin = "no";
+      extraConfig = 
+        ''
+          AllowUsers ${concatMapStrings (user: ''${user} '') cfg.allowed_users}
+        '';
+    };
+    # Add Mosh & allow Mosh ports :)
+    environment.systemPackages = [ pkgs.mosh ];
+    networking.firewall.allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
+  };
 }
