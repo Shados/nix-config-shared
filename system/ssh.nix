31e1a5e2f34c0ee0c7fdf5bd253448813e584c1d
Alexei Robyn <shados@shados.net>
2014-08-11 13:17:22 +1000

    Added programs.ssh.{globalHosts,defaultPort} support to configure ssh_config

--- system/ssh.nix
+++ system/ssh.nix
@@ -3,18 +3,78 @@
 with pkgs.lib;
 
 let
+
   cfg = config.services.openssh;
+  clcfg = config.programs.ssh;
+
+  sshHostOpts = { name, ... }: {
+    options = {
+      name = mkOption {
+        type = types.str;
+        description = ''
+          The name of the ssh host. If undefined, the name of the attribute set will be used.
+        '';
+      };
+      hostName = mkOption {
+        type = types.str;
+        description = ''
+          The hostname of the ssh host.
+        '';
+      };
+      port = mkOption {
+        type = types.nullOr types.int;
+        description = ''
+          The port of the ssh host, leave black to use ssh_config default.
+        '';
+        default = null;
+      };
+      keyFile = mkOption {
+        type = types.nullOr types.path;
+        description = ''
+          The path to the ssh key to use for this host, leave blank if not using key-based authentication or if you want to specify this on the command line, or via default key.
+        '';
+        default = null;
+      };
+    };
+    config = {
+      name = mkDefault name;
+    };
+  };
+
+
+  globalHosts = map (h: getAttr h clcfg.globalHosts) (attrNames clcfg.globalHosts);
+
 in
 
 {
-  options.services.openssh = {
-    allowed_users = mkOption {
-      description = ''
-        A list of the users allowed to log in via SSH.
-      '';
-      default = [ "shados" ];
-      type = types.nullOr (types.listOf types.str);
+  options = {
+    services.openssh = {
+      allowed_users = mkOption {
+        description = ''
+          A list of the users allowed to log in via SSH.
+        '';
+        default = [ "shados" ];
+        type = types.nullOr (types.listOf types.str);
+      };
     };
+    programs.ssh = {
+      globalHosts = mkOption {
+        description = ''
+          Global list of configured ssh_config Host entries, applied to each user. Can reference private keys in the Nix store.
+        '';
+        default = {};
+        type = types.loaOf types.optionSet;
+        options = [ sshHostOpts ];
+      };
+      defaultPort = mkOption {
+        description = ''
+          Default port to attempt to connect to remote hosts on.
+        '';
+        default = 54201;
+        type = types.int;
+      };
+    };
+
   };
 
   config = {
@@ -33,5 +93,20 @@ in
     # Add Mosh & allow Mosh ports :)
     environment.systemPackages = [ pkgs.mosh ];
     networking.firewall.allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
+
+    programs.ssh.extraConfig = ''
+      Match all
+        Port ${toString clcfg.defaultPort}
+      ${flip concatMapStrings globalHosts (host: ''
+        Host ${host.name}
+          ${optionalString (host.port != null) ''Port ${toString host.port}''}
+          ${optionalString (host.keyFile != null) ''IdentityFile ${host.keyFile}''}
+      '')}
+    '';
+
+    programs.ssh.globalHosts.test = {
+      hostName = hostname.shados.net;
+      port = 54201;
+    };
   };
 }
