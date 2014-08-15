3be428dc6adaf039ef66d57b625f55bbe2215dc0
Alexei Robyn <shados@shados.net>
2014-08-15 18:26:07 +1000

    Removed datacoin; coin appears dead. Le sigh.

--- bespoke/pkgs/default.nix
+++ bespoke/pkgs/default.nix
@@ -2,9 +2,6 @@
 
 {
   nixpkgs.config.packageOverrides = pkgs: with pkgs; rec {
-    datacoin-hp = callPackage ./datacoin-hp-git.nix { 
-      libdb = callPackage ./libdb.nix {}; 
-    };
     hah = callPackage ./hah {};
     hostapd = callPackage ./hostapd-git {};
   };
