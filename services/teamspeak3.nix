3fd031ea95c997af7e9eb07689121db08c06e889
Alexei Robyn <shados@shados.net>
2014-08-24 17:45:16 +1000

    Updated ts3 module with sn-specific defaults

--- services/teamspeak3.nix
+++ services/teamspeak3.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }:
 
 pkgs.lib.mkIf config.services.teamspeak3.enable {
-  networking.firewall.allowedTCPPorts = [ 54203 ];
+  networking.firewall = {
+    allowedTCPPorts = [ 54203 ];
+    allowedUDPPorts = [ 54203 ];
+  };
+  services.teamspeak3 = {
+    dataDir = "/srv/teamspeak3-server";
+  };
 }
