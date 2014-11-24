4d60e015d256cb86be9fd3a1563759b9c6eb182c
Alexei Robyn <shados@shados.net>
2014-11-24 14:27:47 +1100

    Fixed gitolite ssh user entry

--- system/ssh-globalhosts.nix
+++ system/ssh-globalhosts.nix
@@ -4,7 +4,7 @@
   programs.ssh.globalHosts = {
     # Host entry for the SN NixOS configuration remote git repo
     gitolite = {
-      hostName = "sns4.shados.net";
+      hostName = "git.shados.net";
       user = "gitolite";
       port = 54201;
       keyFile = "/etc/nixos/modules/keys/nixos-config@shados.net.id_ecdsa";
