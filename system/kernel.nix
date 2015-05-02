76fb18f78b28c000d2c1d5f9d8b793e12600eb07
Alexei Robyn <shados@shados.net>
2015-05-03 01:03:38 +1000

    kernel: version bump

--- system/kernel.nix
+++ system/kernel.nix
@@ -2,9 +2,9 @@
 
 lib.mkIf config.fragments.readahead.enable {
   # Kernel customization
-  boot.kernelPackages = pkgs.linuxPackages_3_15;
+  boot.kernelPackages = pkgs.linuxPackages_3_19;
   nixpkgs.config.packageOverrides = pkgs: {
-    linux_3_15 = pkgs.linux_3_15.override {
+    linux_3_19 = pkgs.linux_3_19.override {
       extraConfig =
         ''
           # Required for systemd-readahead support
