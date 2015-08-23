5733e88f7d5e11e8b13c36db532727a4babf17e2
Alexei Robyn <shados@shados.net>
2015-08-23 19:47:31 +1000

    Added debug option to quassel service.

--- services/quassel.nix
+++ services/quassel.nix
@@ -1,13 +1,31 @@
 { config, pkgs, lib, ... }:
 
-lib.mkIf config.services.quassel.enable {
-  services.quassel = {
-    interface = "0.0.0.0";
-    dataDir = "/srv/quassel/.config/quassel-irc.org";
+let
+  quassel = pkgs.quasselDaemon_qt5;
+  cfg = config.services.quassel;
+in
+
+{
+  options = {
+    services.quassel.debug = lib.mkOption {
+      default = false;
+      description = ''
+        Whether or not to enable debug output.
+      '';
+    };
   };
-  systemd.services.quassel = {
-    requires = [ "postgresql.service" ];
-    serviceConfig.Restart = "on-failure";
+  config = lib.mkIf cfg.enable {
+    services.quassel = {
+      interface = "0.0.0.0";
+      dataDir = "/srv/quassel/.config/quassel-irc.org";
+    };
+    systemd.services.quassel = {
+      requires = [ "postgresql.service" ];
+      serviceConfig.Restart = "on-failure";
+      serviceConfig.ExecStart = lib.mkForce "${quassel}/bin/quasselcore --listen=${cfg.interface} --port=${toString cfg.portNumber} --configdir=${cfg.dataDir} ${lib.optionalString cfg.debug "-d"}";
+    };
+    networking.firewall.allowedTCPPorts = [ 4242 ];
+
   };
-  networking.firewall.allowedTCPPorts = [ 4242 ];
+
 }
