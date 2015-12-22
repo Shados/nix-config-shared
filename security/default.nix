17149d6375874693e974fa459a8ceca1034cc19d
Alexei Robyn <shados@shados.net>
2015-12-22 16:22:12 +1100

    Added some default ulimits.

--- security/default.nix
+++ security/default.nix
@@ -1,8 +1,17 @@
-# Security-focused modules
+# Security-focused modules and defaults
 { config, pkgs, ... }:
 
 {
   imports = [
     ./firewall.nix
+  ];
+
+  security.pam.loginLimits = [
+    { domain = "*"; type = "-"; item = "rtprio"; value = "0"; }
+    { domain = "*"; type = "-"; item = "nice"; value = "0"; }
+    { domain = "*"; type = "-"; item = "noproc"; value = "10240"; }
+    { domain = "*"; type = "-"; item = "nofile"; value = "500000"; }
+    { domain = "@audio"; type = "-"; item = "rtprio"; value = "99"; }
+    { domain = "@audio"; type = "-"; item = "nice"; value = "-20"; }
   ];
 }
