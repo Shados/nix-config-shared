db78bc9fdaa5064571ecefdf8543a6c907e79a6a
Alexei Robyn <shados@shados.net>
2016-03-25 21:58:47 +1100

    Changed default shados shell to fish.

--- system/users.nix
+++ system/users.nix
@@ -14,7 +14,7 @@
       "grsecurity"
     ];
     uid = 1000;
-    useDefaultShell = true;
+    shell = "/run/current-system/sw/bin/fish";
     openssh.authorizedKeys.keyFiles = [ 
       "/etc/nixos/modules/keys/shados@nhnt.shados.net.id_ecdsa.pub"
       "/etc/nixos/modules/keys/shados@sn-u1-malkieri.id_ecdsa.pub"
@@ -23,4 +23,14 @@
   };
   security.initialRootPassword = "$6$t6l9e3mAk$rohvE4HsBPPbsy1pQmtZSVvYUX0Gjl.seA/h6xYiKHc5ZSug0HAe/4F1EDq8XO.7aRrnfv2f9eDMf4kGKIDQ6/";
   services.openssh.allowed_users = [ "shados" ];
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      source $HOME/.config/fish/config.fish
+    '';
+  };
+  environment.systemPackages = with pkgs; [ 
+    which # Fish doesn't have `which` by default
+    bashInteractive # issue #4260
+  ]; 
 }
