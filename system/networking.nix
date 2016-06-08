b5ef584bfdf87e0bb21c18861f8c6fd24253661f
Alexei Robyn <shados@shados.net>
2016-06-08 15:12:42 +1000

    Removed OpenDNS default NS.

--- system/networking.nix
+++ system/networking.nix
@@ -1,10 +1,5 @@
 { config, pkgs, lib, ... }:
 {
-  # Default to OpenDNS nameservers
-  networking.nameservers = lib.mkDefault [
-    "208.67.220.220"
-    "208.67.222.222"
-  ];
   boot.kernel.sysctl = {
     "net.core.default_qdisc" = "fq_codel";
   };
