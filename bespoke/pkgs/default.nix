14039089f30e5eae44f9e252e196e4da567b8519
Alexei Robyn <shados@shados.net>
2016-07-12 16:52:30 +1000

    rssh: Removed, have upstream'd :>

--- bespoke/pkgs/default.nix
+++ bespoke/pkgs/default.nix
@@ -4,7 +4,6 @@
   nixpkgs.config.packageOverrides = pkgs: with pkgs; rec {
     hostapd = callPackage ./hostapd-git {};
     hugo = callPackage ./hugo.nix {};
-    rssh = callPackage ./rssh {};
     rxvt_unicode = callPackage ./urxvt24bit.nix {
       perlSupport = true;
       gdkPixbufSupport = true;
