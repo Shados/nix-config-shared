6fafd7999c504846d2165c5e2852c904c4456a7a
Alexei Robyn <shados@shados.net>
2017-04-16 16:52:30 +1000

    nftables-firewall: networking.firewall support mostly implemented.

--- bespoke/services/nft-firewall.nix
+++ bespoke/services/nft-firewall.nix
@@ -1,3 +1,6 @@
+# pingLimit semantics changed
+# TODO: Modify libvirtd to take libvirt package as a config option, so we can pass it an overrided version that uses iptables-compat and ebtables-compat from nftables package
+# TODO: checkReversePath support
 { config, lib, pkgs, ... }:
 
 with lib;
@@ -10,12 +13,13 @@ let
   chainOpts = { config, name, ... }: let cfg = config; in let config = parentConfig; in {
     options = {
       name = mkOption {
-        example = "input";
+        example = "INPUT";
         type = types.str;
         description = "Name of the nftables chain.";
       };
 
       definition = mkOption {
+        default = "";
         example = "type filter hook input priority 0;";
         type = types.str;
         description = "Defining properties of the chain.";
@@ -28,8 +32,6 @@ let
         type = types.lines;
         description = "Rules to add to the chain.";
       };
-    };
-    config = {
     };
   };
 
@@ -52,8 +54,6 @@ let
           chain.
         '';
       };
-    };
-    config = {
     };
   };
 
@@ -125,6 +125,7 @@ in
     (mkIf cfg.enable {
       # Disable the default firewall & NAT implementations
       networking.firewall.enable = false;
+      networking.firewall.trustedInterfaces = [ "lo" ];
       networking.nat.enable = false;
 
       environment.systemPackages = with pkgs; [ nftables ];
@@ -136,28 +137,85 @@ in
         inet = {
           filter = {
             chains = {
-              input = {
+              nixos-fw-accept.rules = ''
+                # This chain just accepts packets
+                accept
+              '';
+              nixos-fw-refuse.rules = ''
+                # This chain rejects or drops packets
+                ${if fwcfg.rejectPackets then ''
+                  # Send a reset for existing TCP connections
+                  tcp flags != syn reject with tcp reset
+                  # Send ICMP 'port unreachable' for all else
+                  ip reject with icmp type port-unreachable
+                  ip6 reject with icmpv6 type port-unreachable
+                '' else ''
+                  drop
+                ''}
+              '';
+              nixos-fw-log-refuse.rules = ''
+                # This chain optionally performs logging, then 
+                # jumps to the refuse chain
+                ${optionalString fwcfg.logRefusedConnections ''
+                  tcp flags syn log level info prefix "rejected connection: "
+                ''}
+                ${optionalString (fwcfg.logRefusedPackets && !fwcfg.logRefusedUnicastsOnly) ''
+                  meta pkttype broadcast log level info prefix "rejected broadcast: "
+                  meta pkttype multicast log level info prefix "rejected multicast: "
+                  meta pkttype != unicast jump nixos-fw-refuse
+                ''}
+                ${optionalString fwcfg.logRefusedPackets ''
+                  log level info prefix "rejected packet: "
+                ''}
+                jump nixos-fw-refuse
+              '';
+              nixos-fw.rules = ''
+                # Accept all traffic on the trusted interfaces
+                ${flip concatMapStrings fwcfg.trustedInterfaces (iface: ''
+                  meta iifname "${iface}" jump nixos-fw-accept
+                '')}
+
+                # Accept packets from established/related connections
+                ct state established,related accept
+
+                # Accept connections to allowed TCP ports
+                ${concatMapStrings (port: ''
+                  tcp dport ${toString port} jump nixos-fw-accept
+                '') fwcfg.allowedTCPPorts}
+
+                # Accept connections to allowed TCP port ranges
+                ${concatMapStrings (rangeAttr:
+                  let range = toString rangeAttr.from + "-" + toString rangeAttr.to; in ''
+                  tcp dport ${range} jump nixos-fw-accept
+                '') fwcfg.allowedTCPPortRanges}
+
+                # Accept connections to allowed UDP ports
+                ${concatMapStrings (port: ''
+                  udp dport ${toString port} jump nixos-fw-accept
+                '') fwcfg.allowedUDPPorts}
+
+                # Accept connections to allowed UDP port ranges
+                ${concatMapStrings (rangeAttr:
+                  let range = toString rangeAttr.from + "-" + toString rangeAttr.to; in ''
+                  udp dport ${range} jump nixos-fw-accept
+                '') fwcfg.allowedUDPPortRanges}
+
+                # Optionally respond to ICMPv4 pings
+                ${optionalString fwcfg.allowPing ''
+                  ip6 nexthdr icmpv6 icmpv6 type echo-request ${optionalString (fwcfg.pingLimit != null) "limit rate ${fwcfg.pingLimit}"} accept
+                  ip protocol icmp icmp type echo-request ${optionalString (fwcfg.pingLimit != null) "limit rate ${fwcfg.pingLimit}"} accept
+                ''}
+
+                # Filter rules
+
+                # Reject/drop everything else
+                jump nixos-fw-log-refuse
+              '';
+              INPUT = {
                 definition = "type filter hook input priority 0; policy drop";
                 rules = ''
-                  # established/related connections
-                  ct state established,related accept
-
-                  # invalid connections
-                  ct state invalid drop
-
-                  # loopback interface
-                  iif lo accept
-
-                  # ICMP
-                  # routers may also want: mld-listener-query, nd-router-solicit
-                  ip6 nexthdr icmpv6 icmpv6 type { destination-unreachable, packet-too-big, time-exceeded, parameter-problem, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept
-                  ip protocol icmp icmp type { destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
-
-                  # SSH (port 22)
-                  tcp dport ssh accept
-
-                  # HTTP (ports 80 & 443)
-                  tcp dport {http, https} accept
+                  # Enable NixOS' default firewall
+                  jump nixos-fw
                 '';
               };
             };
