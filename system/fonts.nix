3640a9a15c4b2bd4689b235a8d801bb8e419bf40
Alexei Robyn <shados@shados.net>
2018-02-08 07:56:05 +1100

    Optimize font selection by only using 1 nerdfont
    Rather than just installing the entire lot by default.

--- system/fonts.nix
+++ system/fonts.nix
@@ -1,19 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # TODO: only bother enabling for systems with X >.>
-  fonts = {
-    fonts = with pkgs; [
-      corefonts # Microsoft free fonts
-      cm_unicode
-      stix-otf
-      dejavu_fonts
-      ipafont
-      source-code-pro
-      nerdfonts
-    ];
-    # fontconfig = {
-    #   dpi = 120;
-    # };
-  };
+  fonts.fonts = with pkgs; [
+    corefonts # Microsoft free fonts
+    cm_unicode
+    stix-otf
+    dejavu_fonts
+    ipafont
+    source-code-pro
+    (nerdfonts.override {withFont = "FantasqueSansMono";})
+  ];
 }
