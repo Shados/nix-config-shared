39e74c133bf3ba92b6938b0c4ba1ac7aac809ac7
Alexei Robyn <shados@shados.net>
2018-03-21 10:50:43 +1100

    Remove neovim ruby regression work-around

--- apps/vim.nix
+++ apps/vim.nix
@@ -6,12 +6,4 @@
     silver-searcher # `ag`
     universal-ctags
   ];
-  nixpkgs.config.packageOverrides = pkgs: with pkgs; {
-      # Work around regression introduced by
-      # ba58b425f10854cc4d7fb2dce9fbb79d09f882e8 2018-02-22
-      # TODO remove this later
-      neovim = pkgs.neovim.override {
-        withRuby = false;
-      };
-  };
 }
