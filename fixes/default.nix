985256d9365ab7d388d8c8b1ec2886f9cc1e8a45
Alexei Robyn <shados@shados.net>
2018-03-21 11:09:47 +1100

    Add existing-version-check to python-neovim fix

--- fixes/default.nix
+++ fixes/default.nix
@@ -12,31 +12,28 @@ with lib;
     # 2018-03-21: recent versions of deoplete need python-neovim >= 0.2.4
     # TODO remove after commmit 366c79e17f212e581d16a17ca67eb186fd005c61 is in channel
     {
-      nixpkgs.config.packageOverrides = pkgs: with pkgs; {
-        neovim = let
-          newer_python_neovim = packages: rec {
-            pname = "neovim";
-            version = "0.2.4";
-            src = packages.fetchPypi {
-              inherit pname version;
-              sha256 = "0accfgyvihs08bwapgakx6w93p4vbrq2448n2z6gw88m2hja9jm3";
-            };
+      nixpkgs.config.packageOverrides = let
+        python_override = localpython: localpython.override {
+          packageOverrides = self: super: {
+            neovim = super.neovim.overrideAttrs (oldAttrs: rec {
+              pname = "neovim";
+              version = "0.2.4";
+              src = super.fetchPypi {
+                inherit pname version;
+                sha256 = "0accfgyvihs08bwapgakx6w93p4vbrq2448n2z6gw88m2hja9jm3";
+              };
+            });
           };
-          fixedPython3 = pkgs.python3.override {
-            packageOverrides = self: super: {
-              neovim = super.neovim.overrideAttrs (oldAttrs: newer_python_neovim super);
-            };
-          };
-          fixedPython2 = pkgs.python2.override {
-            packageOverrides = self: super: {
-              neovim = super.neovim.overrideAttrs (oldAttrs: newer_python_neovim super);
-            };
-          };
-          wrapNeovim = pkgs.wrapNeovim.override {
-            pythonPackages = fixedPython2.pkgs;
-            python3Packages = fixedPython3.pkgs;
-          };
-        in wrapNeovim pkgs.neovim-unwrapped { };
+        };
+        fixedWrapNeovim = pkgs.wrapNeovim.override {
+          pythonPackages = (python_override pkgs.python2).pkgs;
+          python3Packages = (python_override pkgs.python3).pkgs;
+        };
+      in pkgs: {
+        neovim = if (versionOlder (getVersion pkgs.pythonPackages.neovim) "0.2.4") then
+          fixedWrapNeovim pkgs.neovim-unwrapped { }
+        else
+          pkgs.neovim;
       };
     }
   ];
