77f1701ef3ae3327a6651d349a57481fc75eafb7
Alexei Robyn <shados@shados.net>
2018-04-13 12:02:13 +1000

    Add extra functionality to snap package

--- bespoke/pkgs/snap.nix
+++ bespoke/pkgs/snap.nix
@@ -1,7 +1,49 @@
-{ writeScriptBin, fish, maim, xdotool, pastebin }:
+{ stdenv, writeScriptBin, fish, maim, xdotool, libnotify, pastebin }:
 
-writeScriptBin "snap" ''
-#!${fish}/bin/fish
+let
+  screenshot_dir = "~/technotheca/artifacts/media/images/screenshots";
+  bin = {
+    maim = "${maim}/bin/maim";
+    pb = "${pastebin}/bin/pastebin";
+    xdot = "${xdotool}/bin/xdotool";
+    notify = "${libnotify}/bin/notify-send";
+  };
 
-${maim}/bin/maim -i (${xdotool}/bin/xdotool getactivewindow) | ${pastebin}/bin/pastebin
-''
+  mkSnapScript = name: args: writeScriptBin name (let
+    mapAttrsToStringSep = sep: mapFn: attrs:
+      stdenv.lib.concatStringsSep sep (stdenv.lib.mapAttrsToList mapFn attrs);
+    mkArgFlag = argname: argval: "-${argname}${if (! isNull argval) then " ${argval}" else ""}";
+    mkSnapLine = args:
+      "${bin.maim} ${mapAttrsToStringSep " " mkArgFlag args}";
+  in ''
+    #!${fish}/bin/fish
+
+    if test (count $argv) -gt 0
+      if test $argv[1] = "-l"
+        set -l ss_path ${screenshot_dir}/(hostname)-(date +%F-%T).png
+        ${mkSnapLine args} $ss_path | ${bin.notify} -t 3000 "Screenshot saved to $ss_path"
+      else
+        echo "Unrecognized argument $argv[1]"
+      end
+    else
+      ${mkSnapLine args} | ${bin.pb}
+    end
+  '');
+
+  scripts = [
+    (mkSnapScript "snap" { i = "(${bin.xdot} getactivewindow)"; })
+    (mkSnapScript "snapregion" { s = null; })
+  ];
+in
+stdenv.mkDerivation rec {
+  name = "snap-utils";
+
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin/
+    ${stdenv.lib.concatMapStringsSep "\n" (script: ''
+      cp -r ${script}/bin/* $out/bin/
+    '') scripts}
+    chmod +x $out/bin/*
+  '';
+}
