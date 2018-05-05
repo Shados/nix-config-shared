9b5555ef395e012297f40f1e3dd7629693a42ba8
Alexei Robyn <shados@shados.net>
2018-05-05 23:47:46 +1000

    portable profile: +a zfs option, -duplicate lines

--- profiles/portable/default.nix
+++ profiles/portable/default.nix
@@ -1,7 +1,10 @@
 # Portable / USB installation setup
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-with pkgs.lib;
+with lib;
+let
+  cfg = config.profiles.portable;
+in
 {
   # Import a bunch of stuff from the livecd/installer expressions, piggyback off of others' work :>
   imports = [
@@ -12,51 +15,67 @@ with pkgs.lib;
     <nixpkgs/nixos/modules/profiles/base.nix> 
   ];
 
-  # Stuff needed to boot
-  boot = {
-    # Support kernel-cmdline-configurable root-mounting-delay
-    initrd.postDeviceCommands = pkgs.lib.mkBefore ''
-      rootDelay=0
-      for o in $(cat /proc/cmdline); do
-        case $o in
-          rootdelay=*)
-            set -- $(IFS==; echo $o)
-            rootDelay=$2
-            ;;
-        esac
-      done
+  options = {
+    profiles.portable = {
+      zfsMemoryLimit = mkOption {
+        description = ''
+          Amount of RAM to limit ZFS to using for the ARC, in bytes. Set to 0
+          to disable.
 
-      echo "Sleeping for Root Delay = $rootDelay seconds"
-      sleep $rootDelay # Force artificial boot delay
-    '';
-
-    initrd.kernelModules = [ "ohci_pci" "ehci_pci" "xhci_pci" "uas" "sd_mod" ];
-
-    initrd.supportedFilesystems = [ "zfs" ]; # When don't I use zfs these days?
-
-    kernelParams = [
-      "zfs.zfs_arc_max=536870912"  # Limit ZFS ARC size to 512MB max by default, doesn't hurt for this usage and can be tweaked at boot anyway
-      "boot.shell_on_fail"
-      "rootdelay=1" # Seems we do actually need some level of root delay for usb enumeration to finish, in the real-world. 1s seems OK thus far...
-    ];
-
-    loader = {
-      # NOTE: Still need to set grub.device elsewhere if we're supporting BIOS booting as well
-      grub = {
-        enable = true;
-        efiSupport = mkDefault true;
-        zfsSupport = true;
-      };
-      efi = {
-        canTouchEfiVariables = false;
-        efiSysMountPoint = "/boot";
+          Defaults to 512MB.
+        '';
+        type = types.int;
+        default = 536870912;
       };
     };
   };
 
-  # Setup for quad-core systems as some sort of default, given what I usually end up booting this on
-  nix = {
-    maxJobs = mkDefault 4;
-    buildCores = mkDefault 4;
+  config = {
+    # Stuff needed to boot
+    boot = {
+      # Support kernel-cmdline-configurable root-mounting-delay
+      initrd.postDeviceCommands = pkgs.lib.mkBefore ''
+        rootDelay=0
+        for o in $(cat /proc/cmdline); do
+          case $o in
+            rootdelay=*)
+              set -- $(IFS==; echo $o)
+              rootDelay=$2
+              ;;
+          esac
+        done
+
+        echo "Sleeping for Root Delay = $rootDelay seconds"
+        sleep $rootDelay # Force artificial boot delay
+      '';
+
+      initrd.kernelModules = [ "ohci_pci" "ehci_pci" "xhci_pci" "uas" "sd_mod" ];
+
+      initrd.supportedFilesystems = [ "zfs" ]; # When don't I use zfs these days?
+
+      kernelParams = [
+        "boot.shell_on_fail"
+        "rootdelay=1" # Seems we do actually need some level of root delay for usb enumeration to finish, in the real-world. 1s seems OK thus far...
+      ] ++ optional (cfg.zfsMemoryLimit > 0) "zfs.zfs_arc_max=${toString cfg.zfsMemoryLimit}";
+
+      loader = {
+        # NOTE: Still need to set grub.device elsewhere if we're supporting BIOS booting as well
+        grub = {
+          enable = true;
+          efiSupport = mkDefault true;
+          zfsSupport = true;
+        };
+        efi = {
+          canTouchEfiVariables = false;
+          efiSysMountPoint = "/boot";
+        };
+      };
+    };
+
+    # Setup for quad-core systems as some sort of default, given what I usually end up booting this on
+    nix = {
+      maxJobs = mkDefault 4;
+      buildCores = mkDefault 4;
+    };
   };
 }
