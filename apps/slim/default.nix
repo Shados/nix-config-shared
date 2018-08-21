2573ffea4eba8954cf4ae5ed2c6579d3197e57db
Alexei Robyn <shados@shados.net>
2018-08-21 14:06:39 +1000

    Force slimlock to disable DPMS
    Avoids some issues around dodgy HP docks...

--- apps/slim/default.nix
+++ apps/slim/default.nix
@@ -1,7 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.displayManager.slim = {
-    theme = ./shadosnet-nixos-slim-theme.tar.gz;
+  services.xserver.displayManager = {
+    slim = {
+      theme = ./shadosnet-nixos-slim-theme.tar.gz;
+    };
+  };
+  environment.sessionVariables = {
+    SLIM_CFGFILE = toString (pkgs.writeText "slim_with_lock.conf" (
+      (builtins.readFile config.services.xserver.displayManager.job.environment.SLIM_CFGFILE)
+      + ''
+        dpms_standby_timeout 0
+        dpms_off_timeout 0
+        bell 0
+      ''
+    ));
+    SLIM_THEMESDIR = toString config.services.xserver.displayManager.job.environment.SLIM_THEMESDIR;
   };
 }
