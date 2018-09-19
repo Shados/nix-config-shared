878dc22f8825561ddfa30c429bbb9f7f7f2c804e
Alexei Robyn <shados@shados.net>
2018-09-19 22:11:16 +1000

    postgresql: Have the settings use mkDefault

--- services/postgresql.nix
+++ services/postgresql.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 lib.mkIf config.services.postgresql.enable {
   services.postgresql = {
-    package = pkgs.postgresql94; # TODO: migrate my shit to a more recent postgres and update the default here
-    dataDir = "/srv/postgresql94";
+    package = lib.mkDefault pkgs.postgresql94; # TODO: migrate my shit to a more recent postgres and update the default here
+    dataDir = lib.mkDefault "/srv/postgresql94";
   };
   systemd.services.postgresql = {
     serviceConfig.Restart = "on-failure";
