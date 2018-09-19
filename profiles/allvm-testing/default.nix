09065412a4fd7cc3b164f7e97eb8dd047ff64d3a
Alexei Robyn <shados@shados.net>
2018-09-19 22:20:23 +1000

    profile: allvm-testing: edit for nix settings...
    ...change made in last commit.

--- profiles/allvm-testing/default.nix
+++ profiles/allvm-testing/default.nix
@@ -2,15 +2,10 @@
 {
   nix = {
     binaryCaches = [
-      "https://cache.nixos.org/"
       "https://allvm.cachix.org"
     ];
     binaryCachePublicKeys = [
       "allvm.cachix.org-1:nz7VuSMfFJDKuOc1LEwUguAqS07FOJHY6M45umrtZdk="
-    ];
-    trustedUsers = [
-      "root"
-      "shados"
     ];
   };
 }
