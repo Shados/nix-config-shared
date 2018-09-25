7ae0c509519cbf489795adb405c1e78ab6a97708
Alexei Robyn <shados@shados.net>
2018-09-25 11:34:54 +1000

    vim: Minor improvements.
    - Changed config path (namespaced it).
    - Re-arranged expression for clarity improvements.
    - Defaulted empty lists to expansion to make later editing slightly
      easier.

--- apps/vim.nix
+++ apps/vim.nix
@@ -3,7 +3,101 @@
 with lib;
 
 let
-  cfg = config.programs.neovim.sn;
+  cfg = config.sn.programs.neovim;
+
+  langPkgDefs = with pkgs; {
+    bash = {
+      minimal = [
+        shellcheck
+        shfmt
+      ];
+      full = [
+        bash-language-server
+      ];
+    };
+    c = {
+      minimal = [
+      ];
+      full = [
+        clangd
+      ];
+    };
+    css = {
+      minimal = [
+      ];
+      full = [
+        vscode-css-langserver
+      ];
+    };
+    elm = with elmPackages; {
+      minimal = [
+        ["elm-make" elm] # elm-make for linting
+        elm-format
+      ];
+      full = [
+      ];
+    };
+    go = {
+      minimal = [
+        ["gofmt" go]
+        gocode
+      ];
+      full = [
+        go-langserver
+      ];
+    };
+    haskell = {
+      minimal = [
+      ];
+      full = [
+        # use hie-wrapper to launch correct HIE binary for project's GHC?
+        ["hie-wrapper" hies]
+      ];
+    };
+    lua = with lua52Packages; {
+      minimal = [
+        ["luac" lua]
+        luacheck
+      ];
+      full = [
+      ];
+    };
+    python = with python36Packages; {
+      minimal = [
+        flake8 # Linting/static checking
+        black # Strict style auto-formatting
+        isort # Auto-sorts import statements
+      ];
+      full = [
+        # TODO need a solid solution for managing globally-available Python
+        # modules before I can add optional deps properly?...
+        ["pyls" python-language-server]
+        cython # For Cython linting
+      ];
+    };
+    ruby = {
+      minimal = [
+      ];
+      full = [
+        solargraph
+      ];
+    };
+    rust = {
+      minimal = [
+        ["racer" rustracer]
+      ];
+      full = [
+        ["rls" rustup]
+      ];
+    };
+    vim = {
+      minimal = [
+        ["vint" vim-vint] # Vim linter
+      ];
+      full = [
+      ];
+    };
+  };
 
   getPkgName = pkg: let
       parse = drv: (builtins.parseDrvName drv).name;
@@ -58,100 +152,10 @@ let
       minPkgs
     else
       minPkgs ++ fullPkgs;
-
-
-  langPkgDefs = with pkgs; {
-    bash = {
-      minimal = [
-        shellcheck
-        shfmt
-      ];
-      full = [
-        bash-language-server
-      ];
-    };
-    c = {
-      minimal = [];
-      full = [
-        clangd
-      ];
-    };
-    css = {
-      minimal = [];
-      full = [
-        vscode-css-langserver
-      ];
-    };
-    elm = with elmPackages; {
-      minimal = [
-        ["elm-make" elm] # elm-make for linting
-        elm-format
-      ];
-      full = [];
-    };
-    go = {
-      minimal = [
-        ["gofmt" go]
-        gocode
-      ];
-      full = [
-        go-langserver
-      ];
-    };
-    haskell = {
-      minimal = [];
-      full = [
-        # use hie-wrapper to launch correct HIE binary for project's GHC?
-        ["hie-wrapper" hies]
-      ];
-    };
-    lua = with lua52Packages; {
-      minimal = [
-        ["luac" lua]
-        luacheck
-      ];
-      full = [];
-    };
-    python = with python36Packages; {
-      minimal = [
-        flake8 # Linting/static checking
-        black # Strict style auto-formatting
-        isort # Auto-sorts import statements
-      ];
-      full = [
-        # TODO need a solid solution for managing globally-available Python
-        # modules before I can add optional deps properly?...
-        ["pyls" python-language-server]
-        cython # For Cython linting
-      ];
-    };
-    ruby = {
-      minimal = [
-      ];
-      full = [
-        solargraph
-      ];
-    };
-    rust = {
-      minimal = [
-        ["racer" rustracer]
-      ];
-      full = [
-        ["rls" rustup]
-      ];
-    };
-    vim = {
-      minimal = [
-        ["vint" vim-vint] # Vim linter
-      ];
-      full = [
-      ];
-    };
-  };
 in
 {
   options = {
-    programs.neovim.sn = {
+    sn.programs.neovim = {
       defaultLanguageSupport = mkOption {
         type = with types; langSupportType;
         default = "none";
@@ -165,38 +169,48 @@ in
     };
   };
 
-  config = {
-    environment.systemPackages = let
-        pkgToString = pkg: getPkgName pkg;
-        languageSupportPackages = concatLists (mapAttrsToList (mkLangPkgList) langPkgDefs);
-      in with pkgs; [
-        neovim
-        neovim-remote
-        silver-searcher # `ag`
-        universal-ctags
-      ] ++ languageSupportPackages;
+  config = mkMerge [
+    {
+      environment.systemPackages = let
+          pkgToString = pkg: getPkgName pkg;
+          languageSupportPackages = concatLists (mapAttrsToList (mkLangPkgList) langPkgDefs);
+        in with pkgs; [
+          neovim
+          neovim-remote
+          silver-searcher # `ag`
+          universal-ctags
+        ] ++ languageSupportPackages;
 
-    # cachix caches
-    nix = {
-      binaryCaches = [
-        # Haskell IDE Engine
-        "https://hie-nix.cachix.org"
+      # cachix caches
+      nix = {
+        binaryCaches = [
+          # Haskell IDE Engine
+          "https://hie-nix.cachix.org"
+        ];
+        binaryCachePublicKeys = [
+          "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
+        ];
+      };
+      nixpkgs.overlays = [
+        (let
+          rev = "96af698f0cfefdb4c3375fc199374856b88978dc";
+          sha256 = "1ar0h12ysh9wnkgnvhz891lvis6x9s8w3shaakfdkamxvji868qa";
+          # Get sha256 with e.g.: `nix-prefetch-url --unpack https://github.com/domenkozar/hie-nix/archive/96af698f0cfefdb4c3375fc199374856b88978dc.tar.gz`
+        in self: super: {
+          hies = (import (builtins.fetchTarball {
+            url = "https://github.com/domenkozar/hie-nix/archive/${rev}.tar.gz";
+            inherit sha256;
+          }) {}).hies;
+        })
       ];
-      binaryCachePublicKeys = [
-        "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
-      ];
-    };
-    nixpkgs.overlays = [
-      (let
-        rev = "96af698f0cfefdb4c3375fc199374856b88978dc";
-        sha256 = "1ar0h12ysh9wnkgnvhz891lvis6x9s8w3shaakfdkamxvji868qa";
-        # Get sha256 with e.g.: `nix-prefetch-url --unpack https://github.com/domenkozar/hie-nix/archive/96af698f0cfefdb4c3375fc199374856b88978dc.tar.gz`
-      in self: super: {
-        hies = (import (builtins.fetchTarball {
-          url = "https://github.com/domenkozar/hie-nix/archive/${rev}.tar.gz";
-          inherit sha256;
-        }) {}).hies;
-      })
-    ];
-  };
+    }
+    (mkIf (cfg.languageSupport.python != "none") {
+      sn.programs.flake8 = {
+        enable = true;
+        plugins = with pkgs.python36Packages; [
+          flake8-bugbear
+        ];
+      };
+    })
+  ];
 }
