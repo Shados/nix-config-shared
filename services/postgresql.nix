581072db03cdad5ef0ffcc781a5877953b797b63
Alexei Robyn <shados@shados.net>
2018-09-30 15:55:02 +1000

    Fix postgresql defaults to not collide with...
    the module. That said, I'm pretty sure the module should be changed to
    use mkOptionDefault rather than mkDefault, but oh well.

--- services/postgresql.nix
+++ services/postgresql.nix
@@ -2,8 +2,8 @@
 
 lib.mkIf config.services.postgresql.enable {
   services.postgresql = {
-    package = lib.mkDefault pkgs.postgresql94; # TODO: migrate my shit to a more recent postgres and update the default here
-    dataDir = lib.mkDefault "/srv/postgresql94";
+    package = lib.mkOverride 999 pkgs.postgresql94; # TODO: migrate my shit to a more recent postgres and update the default here
+    dataDir = lib.mkOverride 999 "/srv/postgresql94";
   };
   systemd.services.postgresql = {
     serviceConfig.Restart = "on-failure";
