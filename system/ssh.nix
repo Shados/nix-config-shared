f210f7778dae1887e502cb73ba1ea46907c08344
Alexei Robyn <shados@shados.net>
2019-01-21 11:53:54 +1100

    Add 'enableMosh' as an openssh service option

--- system/ssh.nix
+++ system/ssh.nix
@@ -20,6 +20,13 @@ in
         default = [];
         type = types.nullOr (types.listOf types.str);
       };
+      enableMosh = mkOption {
+        type = with types; bool;
+        default = true;
+        description = ''
+          Whether or not to enable Mosh connectivity.
+        '';
+      };
     };
     programs.ssh = {
       globalHosts = mkOption {
@@ -86,49 +93,52 @@ in
 
   };
 
-  config = {
-    services.openssh = {
-      enable = true;
-      # Some options for improved security
-      ports = [ 54201 ]; # Non-default port for security, SSH module automatically adds its ports to the FW
-      passwordAuthentication = false;
-      challengeResponseAuthentication = false;
-      permitRootLogin = "no";
-      extraConfig = ''
-        LogLevel VERBOSE
-        AllowUsers ${concatMapStrings (user: ''${user} '') cfg.allowed_users}
+  config = mkMerge [
+    {
+      services.openssh = {
+        enable = true;
+        # Some options for improved security
+        ports = [ 54201 ]; # Non-default port for security, SSH module automatically adds its ports to the FW
+        passwordAuthentication = false;
+        challengeResponseAuthentication = false;
+        permitRootLogin = "no";
+        extraConfig = ''
+          LogLevel VERBOSE
+          AllowUsers ${concatMapStrings (user: ''${user} '') cfg.allowed_users}
 
-        # Supporting URXVT-256color and other non-standard terms a bit better
-        AcceptEnv TERMINFO
+          # Supporting URXVT-256color and other non-standard terms a bit better
+          AcceptEnv TERMINFO
 
-        # Use within-SSH keepalives; helps to quickly reap failed ssh
-        # connections and is useful for long-living, auto-restarting SSH
-        # tunnels
-        # Send 8 seconds, 3 sequential failures == dead connection
-        ClientAliveInterval 8
-        ClientAliveCountMax 3
+          # Use within-SSH keepalives; helps to quickly reap failed ssh
+          # connections and is useful for long-living, auto-restarting SSH
+          # tunnels
+          # Send 8 seconds, 3 sequential failures == dead connection
+          ClientAliveInterval 8
+          ClientAliveCountMax 3
+        '';
+      };
+
+      programs.ssh.extraConfig = ''
+        ${flip concatMapStrings globalHosts (host: ''
+          Host ${host.name}
+            HostName ${host.hostName}
+            ${optionalString (host.user != null) ''User ${toString host.user}''}
+            ${optionalString (host.port != null) ''Port ${toString host.port}''}
+            ${optionalString (host.keyFile != null) ''IdentityFile ${toString host.keyFile}''}
+            ${optionalString (host.extraConfig != null) host.extraConfig}
+        '')}
+        Match all
+          Port ${toString clcfg.defaultPort}
       '';
-    };
 
-    # Add Mosh & allow Mosh ports :)
-    environment.systemPackages = [ pkgs.mosh ];
-    networking.firewall.allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
-
-    programs.ssh.extraConfig = ''
-      ${flip concatMapStrings globalHosts (host: ''
-        Host ${host.name}
-          HostName ${host.hostName}
-          ${optionalString (host.user != null) ''User ${toString host.user}''}
-          ${optionalString (host.port != null) ''Port ${toString host.port}''}
-          ${optionalString (host.keyFile != null) ''IdentityFile ${toString host.keyFile}''}
-          ${optionalString (host.extraConfig != null) host.extraConfig}
-      '')}
-      Match all
-        Port ${toString clcfg.defaultPort}
-    '';
-
-    # Allow X11 forwarding by default, useful for porting remote clipboard to local
-    # Also needs ForwardX11 and ForwardX11Trusted set on the client side for this host
-    services.openssh.forwardX11 = true;
-  };
+      # Allow X11 forwarding by default, useful for porting remote clipboard to local
+      # Also needs ForwardX11 and ForwardX11Trusted set on the client side for this host
+      services.openssh.forwardX11 = true;
+    }
+    (mkIf cfg.enableMosh {
+      # Add Mosh & allow Mosh ports :)
+      environment.systemPackages = [ pkgs.mosh ];
+      networking.firewall.allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
+    })
+  ];
 }
