01c6ab797c434b3809532a303976c7d183dd3cef
Alexei Robyn <shados@shados.net>
2019-08-31 23:31:08 +1000

    services/wireguard: Workaround issue nixpkgs#63869

--- services/wireguard.nix
+++ services/wireguard.nix
@@ -22,5 +22,28 @@ let
     };
 in
 {
-  systemd.services = listToAttrs (map (modifyWgService) wgInts);
+  config = mkMerge [
+    {
+      systemd.services = listToAttrs (map (modifyWgService) wgInts);
+    }
+    { # Workaround for issue detailed in https://github.com/NixOS/nixpkgs/issues/63869#issuecomment-514655131
+      systemd.services = let
+        allPeers = flatten (mapAttrsToList (n: cfg:
+          map (peer: { intName = n; inherit peer; }) cfg.peers
+        ) config.networking.wireguard.interfaces);
+        keyToUnitName = replaceChars
+          [ "/" "-"    " "     "+"     "="      ]
+          [ "-" "\\x2d" "\\x20" "\\x2b" "\\x3d" ];
+      in listToAttrs (map ({ intName, peer }: nameValuePair
+        "wireguard-${intName}-peer-${keyToUnitName peer.publicKey}"
+        {
+          serviceConfig = {
+            Restart = mkDefault "on-failure";
+            RestartSec = mkDefault 3;
+            Type = mkForce "simple";
+          };
+        }
+      ) allPeers);
+    }
+  ];
 }
