60dd97c0332c8de92c1deaf11256e78b343f544e
Alexei Robyn <shados@shados.net>
2019-09-02 13:02:56 +1000

    fixes: Account for possibly-missing oldAttrs.patches

--- overlays/fixes/default.nix
+++ overlays/fixes/default.nix
@@ -103,7 +103,7 @@
             else super.monero;
           # Remove once we have upstream fix from https://github.com/pimutils/vdirsyncer/pull/788
           vdirsyncer = super.vdirsyncer.overrideAttrs (oldAttrs: rec {
-            patches = oldAttrs.patches ++ [
+            patches = oldAttrs.patches or [] ++ [
               (super.fetchpatch {
                 url = https://github.com/pimutils/vdirsyncer/pull/788.patch;
                 sha256 = "0vl942ii5iad47y63v0ngmhfp37n30nxyk4j7h64b95fk38vfwx9";
