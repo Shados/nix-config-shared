10bcaaf767020d209b7f8d7dd8639139d6ddd873
Alexei Robyn <shados@shados.net>
2020-06-12 13:28:10 +1000

    pins: Update NUR pin

--- pins/default.nix
+++ pins/default.nix
@@ -1,7 +1,7 @@
 {
   nur = let
-    rev = "c31c1ebffd12a04db46869751b5e957df177ae95";
-    sha256 = "0kwd9i49ggd0cqlpl5h9pcjwpm6xkcl86cx1g1spg9g9vljmjr85";
+    rev = "fe3049efdf10d72eea0385c05dfb30fa516346f5";
+    sha256 = "0jmw4lrwhajz4a2r0viy2cp3hzv0n4a1f9ydldawl9s57rc3lnv9";
   in builtins.fetchTarball {
     url = "https://github.com/nix-community/NUR/archive/${rev}.tar.gz";
     inherit sha256;
