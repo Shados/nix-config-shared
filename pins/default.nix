9fb5ad45ead54365bbadaf340d2a0ce23c474845
Alexei Robyn <shados@shados.net>
2020-08-03 14:35:25 +1000

    pins: Bump NUR pin

--- pins/default.nix
+++ pins/default.nix
@@ -1,7 +1,7 @@
 {
   nur = let
-    rev = "b824ad0cbc68e2eb1e25031fc7b29af19a59cc1b";
-    sha256 = "179dw1lciq4ihlxgz1d5b3b41hzz9vldya2m3ampv9wc1a3aqai9";
+    rev = "a35bcadb2c9eabd9f4f84f0c5b50c3ee9b89962b";
+    sha256 = "150m3nl8klmx0xzrrwn0xnh2ahigvd6f8793d9hw51z6anccpp52";
   in builtins.fetchTarball {
     url = "https://github.com/nix-community/NUR/archive/${rev}.tar.gz";
     inherit sha256;
