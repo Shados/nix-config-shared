74c0793e8c75b2638d29636e91d129875217cc5e
Alexei Robyn <shados@shados.net>
2020-11-18 19:53:00 +1100

    profiles/router/dnsmasq: Make use of network registry for static DHCP leases

--- profiles/router/dnsmasq.nix
+++ profiles/router/dnsmasq.nix
@@ -1,19 +1,25 @@
 { config, pkgs, lib, ... }:
-
-let 
+with lib;
+let
   cfg = config.fragments.router;
 in
-
-lib.mkIf (cfg.enable && cfg.enableDhcp) {
+mkIf (cfg.enable && cfg.enableDhcp) {
   services.dnsmasq = {
     enable = true;
     servers = config.networking.nameservers;
-    extraConfig = ''
+    extraConfig = let
+      staticDevs = filter (host: host.mac != null) (attrValues config.registry.network);
+    in ''
       interface=${cfg.intBridge}
       dhcp-range=${cfg.intSubnet + "." + toString (builtins.elemAt cfg.dhcpRange 0)},${cfg.intSubnet + "." + toString (builtins.elemAt cfg.dhcpRange 1)},12h
       # https://serverfault.com/questions/255487/excessive-dhcp-requests-in-var-log-messages-dhcpinform-dhcpack-and-dhcpreques
       dhcp-option=252,"\n"
-    '';
+
+    '' # Static DHCP leases based on MAC addresses
+    + concatStrings (flip map staticDevs (host: ''
+      # Static-mapped host: ${host.name}
+      dhcp-host=${host.mac},${host.ipv4}
+    ''));
   };
 
   networking.nft-firewall.inet.filter.lan-fw.rules = ''
