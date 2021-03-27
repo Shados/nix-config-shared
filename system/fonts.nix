097940f7f59207f8ca9c4f77f414ce3009cf73cf
Alexei Robyn <shados@shados.net>
2021-03-27 14:07:28 +1100

    system/fonts: Only install fonts on local systems

--- system/fonts.nix
+++ system/fonts.nix
@@ -1,12 +1,12 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  fonts.fonts = with pkgs; [
+  fonts.fonts = lib.mkIf (!config.fragments.remote) (with pkgs; [
     corefonts # Microsoft free fonts
     cm_unicode
     stix-otf
     dejavu_fonts
     ipafont
     source-code-pro
-  ];
+  ]);
 }
