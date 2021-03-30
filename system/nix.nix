e85356c75f2ed7f50a566dde35d98658d0b60b9f
Alexei Robyn <shados@shados.net>
2021-03-30 11:03:52 +1100

    system/nix: Remove plugin stuff

--- system/nix.nix
+++ system/nix.nix
@@ -1,18 +1,5 @@
 { config, lib, pkgs, ... }:
 with lib;
-let
-  nix-fetchers = builtins.fetchGit {
-    url = https://github.com/Shados/nix-fetchers;
-    ref = "fetch-pypi-hash-xdg-cache-fix";
-    rev = "55f1edea80ecb81c9f139d1b0e02ae647a23953c";
-  };
-  makeExtraBuiltins = pkgs.callPackage "${nix-fetchers}/make-extra-builtins.nix" { };
-  extraBuiltins = makeExtraBuiltins {
-    fetchers = {
-      fetch-pypi-hash = "${nix-fetchers}/fetch-pypi-hash";
-    };
-  };
-in
 {
   nix = {
     # 0 will auto-detect the number of physical cores and use that
@@ -36,15 +23,5 @@ in
     binaryCaches = mkOrder 999 [
       "https://cache.nixos.org/"
     ];
-
-    extraOptions = ''
-      plugin-files = ${pkgs.nix-plugins}/lib/nix/plugins/libnix-extra-builtins.so
-      extra-builtins-file = ${extraBuiltins}/extra-builtins.nix
-    '';
   };
-
-  environment.systemPackages = [
-    # Install fetch-pypi-hash as a shell command also
-    (pkgs.callPackage "${nix-fetchers}/fetch-pypi-hash/build.nix" { })
-  ];
 }
