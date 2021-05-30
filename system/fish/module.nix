08db187a1e4076748edc07034677c47fecbef28c
Alexei Robyn <shados@shados.net>
2021-05-30 18:05:21 +1000

    system/fish: Fix fish setup on freshly nixos-install'd systems

--- system/fish/module.nix
+++ system/fish/module.nix
@@ -19,8 +19,13 @@ in
   config = mkIf cfg.enable {
     programs.fish = {
       # The functionDirs have to be prefixed to $fish_function_path so that they
-      # can, e.g. override the default fish_prompt function
+      # can, e.g. override the default fish_prompt function.
+      # Including a nix-store version ensures that fish config is working OK
+      # even on a freshly nixos-install'd system, while including the
+      # out-of-store version *in front of* the store version allows dynamically
+      # overriding it on a built system.
       shellInit = mkBefore (concatMapStringsSep "\n" (dir: ''
+        set -p fish_function_path ${dir}
         set -p fish_function_path ${toString dir}
       '') cfg.functionDirs);
     };
