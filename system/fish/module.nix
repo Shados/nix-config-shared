19e1dd9622a470e82ccdab6bead9834fa09e18e5
Alexei Robyn <shados@shados.net>
2021-07-02 14:54:59 +1000

    system/fish: Improve fish_function_path override

--- system/fish/module.nix
+++ system/fish/module.nix
@@ -18,15 +18,16 @@ in
   };
   config = mkIf cfg.enable {
     programs.fish = {
-      # The functionDirs have to be prefixed to $fish_function_path so that they
-      # can, e.g. override the default fish_prompt function.
+      # The functionDirs have to be added early in $fish_function_path so that
+      # they can, e.g. override the default fish_prompt function. We add them
+      # behind the first item because the first item refers to the user's XDG
+      # config dir for fish.
       # Including a nix-store version ensures that fish config is working OK
       # even on a freshly nixos-install'd system, while including the
       # out-of-store version *in front of* the store version allows dynamically
       # overriding it on a built system.
       shellInit = mkBefore (concatMapStringsSep "\n" (dir: ''
-        set -p fish_function_path ${dir}
-        set -p fish_function_path ${toString dir}
+        set fish_function_path $fish_function_path[1] ${toString dir} ${dir} $fish_function_path[2..]
       '') cfg.functionDirs);
     };
   };
