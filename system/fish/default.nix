d67efa51c863dfaba35aa33e2d46907b72cd01bb
Alexei Robyn <shados@shados.net>
2021-07-02 15:14:38 +1000

    system/fish: Move 'v' alias to overridable function

--- system/fish/default.nix
+++ system/fish/default.nix
@@ -68,7 +68,6 @@ in
           ".." = "cd ..";
           "..." = "cd ../..";
           g = "git";
-          v = "nvim";
           whois = "whois -h whois-servers.net";
           # SSH while ignoring host key checking (potentially risky)
           scpbad = "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeychecking=no";
