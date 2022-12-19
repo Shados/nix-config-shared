f4f50864d75342791cc9b9b8d21ba65b2e888f47
Alexei Robyn <shados@shados.net>
2022-12-20 00:01:10 +1100

    system/fish/shados: Remove nixpkgs NIX_PATH override

--- system/fish/shados.env.fish
+++ system/fish/shados.env.fish
@@ -4,7 +4,3 @@
 umask 022
 
 path_prepend $HOME/technotheca/artifacts/packages/bin $HOME/technotheca/packages/bin
-
-if test -d "$HOME/nixpkgs-local";
-  set -p NIX_PATH "nixpkgs=$HOME/nixpkgs-local"
-end
