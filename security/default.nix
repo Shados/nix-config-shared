2a3a7447c2ce4b7b8b9c8a0490bc1562762c6085
Alexei Robyn <shados@shados.net>
2023-02-23 15:01:42 +1100

    security: Update audio-group realtime ulimits config

--- security/default.nix
+++ security/default.nix
@@ -11,6 +11,9 @@
     { domain = "*"; type = "-"; item = "nice"; value = "0"; }
     { domain = "*"; type = "-"; item = "nproc"; value = "10240"; }
     { domain = "*"; type = "-"; item = "nofile"; value = "524288"; }
+    { domain = "@audio"; item = "nofile"; type = "soft"; value = "524288"; }
+    { domain = "@audio"; item = "nofile"; type = "hard"; value = "524288"; }
+    { domain = "@audio"; type = "-"; item = "memlock"; value = "unlimited"; }
     { domain = "@audio"; type = "-"; item = "rtprio"; value = "99"; }
     { domain = "@audio"; type = "-"; item = "nice"; value = "-20"; }
   ];
