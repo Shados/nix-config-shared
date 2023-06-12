4475730285affbc830326372765d29e58f419162
Alexei Robyn <shados@shados.net>
2023-06-12 14:19:28 +1000

    Fix up sendmail/SMTP config

--- system/smtp.nix
+++ system/smtp.nix
@@ -5,15 +5,17 @@ with lib;
     enable = true;
     accounts.default = {
       auth = true;
-      host = "smtp.fastmail.com:465";
+      host = "smtp.fastmail.com";
+      port = 465;
       user = "shados@f-m.fm";
       passwordeval = "cat /etc/nixos/private-keys/fastmail-smtp.pass";
+      tls = true;
+      tls_starttls = false;
+      from = "cronjobs@shados.net";
+      set_from_header = true;
     };
     defaults = {
-      tls = true;
-      tls_starttls = true;
       domain = "shados.net";
-      from = "sn-cronjobs@shados.net";
     };
   };
 }
