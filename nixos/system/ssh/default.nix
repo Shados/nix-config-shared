b8ccc7407d55bea46bbedd5847ef9c1caff30408
Alexei Robyn <shados@shados.net>
2023-07-03 17:10:01 +1000

    nixos/system/ssh: Remove unneeded ssh known_hosts entry

--- nixos/system/ssh/default.nix
+++ nixos/system/ssh/default.nix
@@ -32,15 +32,6 @@
   programs.ssh.knownHosts = {
     "git.shados.net".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO0oGzy9ylQHKEYaH4jJ38QM9nFxiQTF+flQUYbpqbF6";
   };
-  programs.ssh.globalHosts = {
-    # Host entry for the SN NixOS configuration remote git repo
-    gitolite = {
-      hostName = "git.shados.net";
-      user = "gitolite";
-      port = 54201;
-      keyFile = "/etc/nixos/private-keys/nixos-config@shados.net.id_ecdsa";
-    };
-  };
 
   security.pam.enableSSHAgentAuth = true;
 }
