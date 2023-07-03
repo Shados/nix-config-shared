d9519e47ceb26106c3715437bcbbce9ff18c0ecd
Alexei Robyn <shados@shados.net>
2023-07-03 17:09:05 +1000

    nixos/system/smtp: Use a sops secret for the password

--- nixos/system/smtp.nix
+++ nixos/system/smtp.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 with lib;
 {
+  sops.secrets.msmtp-password-fastmail.owner = "shados";
   programs.msmtp = {
     enable = true;
     accounts.default = {
@@ -8,7 +9,7 @@ with lib;
       host = "smtp.fastmail.com";
       port = 465;
       user = "shados@f-m.fm";
-      passwordeval = "cat /etc/nixos/private-keys/fastmail-smtp.pass";
+      passwordeval = "cat ${config.sops.secrets.msmtp-password-fastmail.path}";
       tls = true;
       tls_starttls = false;
       from = "cronjobs@shados.net";
