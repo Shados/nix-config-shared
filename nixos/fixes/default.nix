db8c6fd3c0f2289b9c5c662bdd0eb4eddbb57fdc
Alexei Robyn <shados@shados.net>
2024-02-29 19:03:35 +1100

    Backport an SDDM fix.

--- nixos/fixes/default.nix
+++ nixos/fixes/default.nix
@@ -1,5 +1,17 @@
 { config, inputs, lib, pkgs, ... }:
 {
   nixpkgs.overlays = [
+    (self: super: {
+      libsForQt5 = super.libsForQt5 // {
+        sddm = super.libsForQt5.sddm.overrideAttrs(oa: {
+          patches = [
+            (super.fetchpatch {
+              url = "https://github.com/sddm/sddm/pull/1805.patch";
+              sha256 = "0zqdzq9c5cd1isyh1095rn8qjsm0rwgslg120p3lnpvk4r1azhks";
+            })
+          ];
+        });
+      };
+    })
   ];
 }
