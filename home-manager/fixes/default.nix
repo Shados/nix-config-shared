57efbfd0aa65b65c3992903e57e5830fff4c9e55
Alexei Robyn <shados@shados.net>
2024-07-09 08:31:51 +1000

    home-manager/fixes: Bump obs-studio to 30.2.0-rc1 for WHIP fixes

--- home-manager/fixes/default.nix
+++ home-manager/fixes/default.nix
@@ -8,6 +8,34 @@ with lib;
           "--disable-gpu-accel" # GPU acceleration has been varyingly buggy and slow since ~1.6.0, disable it for now
         ];
       });
+      obs-studio = if versionOlder (getVersion super.obs-studio) "30.2.0" then super.obs-studio.overrideAttrs(oa: rec {
+        version = "30.2.0-rc1";
+        src = super.fetchFromGitHub {
+          owner = "obsproject"; repo = "obs-studio";
+          rev = version;
+          sha256 = "sha256-1y+4cIaRCLhPOvT5nBPt2duEP41JFOfden2q828AYT8=";
+          fetchSubmodules = true;
+        };
+        patches = [
+          (pkgs.path + "/pkgs/applications/video/obs-studio/Enable-file-access-and-universal-access-for-file-URL.patch")
+          ./obs-nix.patch
+
+          # Fix libobs.pc for plugins on non-x86 systems
+          (super.fetchpatch {
+            name = "fix-arm64-cmake.patch";
+            url = "https://git.alpinelinux.org/aports/plain/community/obs-studio/broken-config.patch?id=a92887564dcc65e07b6be8a6224fda730259ae2b";
+            hash = "sha256-yRSw4VWDwMwysDB3Hw/tsmTjEQUhipvrVRQcZkbtuoI=";
+            includes = [ "*/CompilerConfig.cmake" ];
+          })
+        ];
+        buildInputs = oa.buildInputs ++ (with super; [
+          uthash cjson nv-codec-headers-12
+        ]);
+        cmakeFlags = oa.cmakeFlags ++ [
+          "-DENABLE_AJA=0"
+        ];
+      }) else super.obs-studio;
+      wrapOBS = super.callPackage (pkgs.path + "/pkgs/applications/video/obs-studio/wrapper.nix") { inherit (self) obs-studio; };
     })
     (self: super: super.lib.defineLuaPackageOverrides super [(luaself: luasuper: {
       luasystem = luasuper.luaLib.overrideLuarocks luasuper.luasystem (drv: {
