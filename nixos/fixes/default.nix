bea58a05df50bddedd2990affa946b72e0fb9867
Alexei Robyn <shados@shados.net>
2024-10-28 15:26:21 +1100

    nixos/fixes: Workaround clash between mullvad v1 cgroup usage and systemd v2 cgroup usage

--- nixos/fixes/default.nix
+++ nixos/fixes/default.nix
@@ -1,7 +1,15 @@
 { config, inputs, lib, pkgs, ... }:
+let
+  inherit (lib) mkIf mkMerge;
+in
 {
-  nixpkgs.overlays = [
+  config = mkMerge [
+    {
+      # Fix issue in the wake of nixpkgs #336988
+      systemd.services.dhcpcd.serviceConfig.ReadWritePaths = [ "/proc/sys/net/ipv4" ];
+    }
+    (mkIf config.services.mullvad-vpn.enable {
+      systemd.services.mullvad-daemon.environment.TALPID_NET_CLS_MOUNT_DIR = "/run/mullvad-net-cls-v1";
+    })
   ];
-  # Fix issue in the wake of nixpkgs #336988
-  systemd.services.dhcpcd.serviceConfig.ReadWritePaths = [ "/proc/sys/net/ipv4" ];
 }
