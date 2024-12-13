1cb840d65da829aeb88d9bb9df72e091d8d768cd
Alexei Robyn <shados@shados.net>
2024-12-13 13:43:41 +1100

    nixos/system/networking: Default DNS servers to Quad9

--- nixos/system/networking.nix
+++ nixos/system/networking.nix
@@ -1,7 +1,15 @@
 { config, pkgs, lib, ... }:
+let
+  inherit (lib) mkDefault optionals;
+in
 {
   boot.kernel.sysctl = {
     "net.core.default_qdisc" = "fq_codel";
   };
   networking.firewall.logRefusedConnections = false; # Too noisy
+  networking.nameservers = mkDefault ([
+    "9.9.9.9" "149.112.112.112" # Quad9
+  ] ++ optionals config.networking.enableIPv6 [
+    "2620:fe::fe" "2620:fe::9" # Quad9
+  ]);
 }
