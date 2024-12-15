7ba02a342d43073168ceaecc55dc67426361cec8
Alexei Robyn <shados@shados.net>
2024-12-15 15:58:09 +1100

    home-manager/system/direnv/layout_poetry: Make some improvements
    `poetry install --sync` is unfortunately (and needlessly) quite slow, so
    I might want to cache this at some point.

--- home-manager/system/direnv/layout_poetry.sh
+++ home-manager/system/direnv/layout_poetry.sh
@@ -2,20 +2,24 @@
 # shellcheck disable=2016
 # TODO need a declarative version of the `poetry install` command that says "reify virtualenv to current pyproject.toml/poetry.lock state"
 layout_poetry() {
-  if [[ ! -f pyproject.toml ]]; then
-    log_error 'No pyproject.toml found.  Use `poetry new` or `poetry init` to create one first.'
-    exit 2
-  fi
+	if [[ ! -f pyproject.toml ]]; then
+		log_error 'No pyproject.toml found.  Use `poetry new` or `poetry init` to create one first.'
+		exit 2
+	fi
 
-  local VENV
-  VENV=$(poetry env list --full-path | cut -d' ' -f1)
-  if [[ ! -d $VENV || ! -d $VENV/bin ]]; then
-    log_status 'No created poetry virtual environment found. Creating new one...'
-    poetry install || exit 2
-    VENV=$(poetry env list --full-path | cut -d' ' -f1)
-  fi
+	local VENV
+	VENV=$(poetry env info --path)
+	if [[ ! -d $VENV || ! -d $VENV/bin ]]; then
+		log_status 'No created poetry virtual environment found. Creating new one...'
+		poetry install --sync || exit 2
+		VENV=$(poetry env info --path)
+	else
+		log_status 'Synchronising poetry virtual environment to lockfile...'
+		poetry lock --no-update
+		poetry install --sync || exit 2
+	fi
 
-  export VIRTUAL_ENV="$VENV"
-  export POETRY_ACTIVE=1
-  PATH_add "$VENV/bin"
+	export VIRTUAL_ENV="$VENV"
+	export POETRY_ACTIVE=1
+	PATH_add "$VENV/bin"
 }
