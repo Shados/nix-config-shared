634a87e7ea9bc6a64131b1bc8f2c6f043b0412c0
Alexei Robyn <shados@shados.net>
2024-12-19 13:14:37 +1100

    nixos/fixes: Add missing mullvad-early-boot-blocking service

--- nixos/fixes/default.nix
+++ nixos/fixes/default.nix
@@ -58,6 +58,17 @@ in
     }
     (mkIf config.services.mullvad-vpn.enable {
       systemd.services.mullvad-daemon.environment.TALPID_NET_CLS_MOUNT_DIR = "/run/mullvad-net-cls-v1";
+      systemd.services.mullvad-early-boot-blocking = rec {
+        description = "Mullvad early boot network blocker";
+        unitConfig.DefaultDependencies = "no";
+        wants = [ "network-pre.target" ];
+        wantedBy = [ "mullvad-daemon.service" ];
+        before = wants ++ wantedBy;
+        serviceConfig = {
+          ExecStart = "${lib.getExe' config.services.mullvad-vpn.package "mullvad-daemon"} --initialize-early-boot-firewall";
+          Type = "oneshot";
+        };
+      };
     })
   ];
 }
