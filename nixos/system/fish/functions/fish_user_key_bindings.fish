73f94d80e7aa20bd0a9e38a81605e13fc9ceea2e
Alexei Robyn <shados@shados.net>
2025-12-22 21:51:56 +1100

    nixos/system/fish: Update for default keybind changes

--- nixos/system/fish/functions/fish_user_key_bindings.fish
+++ nixos/system/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,15 @@
 function fish_user_key_bindings
-  bind \ej down-or-search
-  bind \ek up-or-search
-  bind \eh backward-word
-  bind \el forward-word
+  bind alt-j down-or-search
+  bind alt-k up-or-search
+  bind alt-h backward-word
+  bind alt-l forward-word
+  bind alt-left prevd-or-backward-word
+  bind alt-right nextd-or-forward-word
+  bind alt-backspace backward-kill-word
+  bind alt-delete kill-word
+  bind ctrl-left backward-token
+  bind ctrl-right forward-token
+  bind ctrl-backspace backward-kill-token
+  bind ctrl-delete kill-token
+  bind ctrl-alt-h backward-kill-word
 end
